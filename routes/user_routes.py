a7ca05373230499cf4b383763afc6a4a3a38738b
Zeeshan Qureshi <zeeshan_qureshi_1@hotmail.com>
2024-06-28 11:05:49 +0500

    Change of Python Docker image and some logs

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -581,13 +581,14 @@ async def start_charging_v2(request: web.Request) -> web.Response:
         #         ),
         #         content_type="application/json",
         #     )
+        LOGGER.info(f'id_tag : {id_tag}')
         is_session_started, msg = await ocpp_server.remote_start(
             charger_id=charger_id,
             id_tag=id_tag,
             connector_id=connector_id,
             tenant_id=tenant_id
         )
-
+        LOGGER.info(f'is session start=> charger_id: {charger_id}, id_tag: {id_tag}, connector_id: {connector_id}, tenant_id: {tenant_id}')
         if is_session_started:
             await fcm.send_notification(
                 title="Session Started",
